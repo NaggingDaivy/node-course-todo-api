bbf12280d392d5b7afe5c775b17341ab6bf1c213
Daivy Merlijs <daivy.merlijs@hotmail.com>
2017-09-27 11:53:11 +0200

    Testing model & schemas

--- server/server.js
+++ server/server.js
@@ -1,48 +1,72 @@
-const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise; //
-
-mongoose.connect('mongodb://localhost:27017/ToDoApp', {useMongoClient: true});
 
 // save new something
 var Todo = mongoose.model('Todo', { // model du document, à mettre au singulier car mongoose mettra le nom de la collec au pluriel
 
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true //remove white space before and after 
 
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
 
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
-var newTodo = new Todo({
-    text: 'Cook dinner'
-});
-
-newTodo.save().then((doc) => {
-    console.log('Saved to do', doc);
-
-}, (err) => {
-    console.log('Unable to save todo : ', err);
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        minlength: 1,
+        required: true,
+        trim: true
+    }
 
 });
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved to do', doc);
+
+// }, (err) => {
+//     console.log('Unable to save todo : ', err);
+
+// });
 
 
 var otherTodo = new Todo({
-    text: 'Feed the cat',
-    completed: true,
-    completedAt: 123
+    text: '    Edit this video    ' // ok because trim, mongoose utilise le cast, taper 123 convertira en '123'
+
 });
 
 otherTodo.save().then((doc) => {
-    console.log(JSON.stringify(doc,undefined,2));
+    console.log(JSON.stringify(doc, undefined, 2));
 
 }, (err) => {
     console.log('Unable to save todo : ', err);
+
+});
+
+
+var user = new User({
+    email: 'daivy.merlijs@hotmail.com      '
+
+})
+
+user.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+
+}, (err) => {
+    console.log('Unable to save user : ', err);
 
 });
