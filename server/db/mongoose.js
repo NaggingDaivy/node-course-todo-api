418d96915083e496736c80f12ec99543462b0a41
Daivy Merlijs <daivy.merlijs@hotmail.com>
2017-09-30 14:02:44 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise; //
 
-mongoose.connect('mongodb://localhost:27017/ToDoApp', {
+var url = process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp'
+
+mongoose.connect(url, {
     useMongoClient: true
 });
 
